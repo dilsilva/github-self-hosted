dd82e8993713c11637f106bac8f7dc08156d052e
Diego Silva <dilsilva.diego@gmail.com>
2024-12-08 23:25:32 +0100

    testing pipeline

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -53,8 +53,8 @@ resource "aws_ecs_task_definition" "task" {
   family                   = "${var.project}-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 256
-  memory                   = 512
+  cpu                      = 512
+  memory                   = 1024
   execution_role_arn       = aws_iam_role.task-execution-role.arn
 
   container_definitions = jsonencode([

edba2862ace4a14526e4b68b09a4861460351e1e
Diego Silva <dilsilva.diego@gmail.com>
2024-12-08 23:47:57 +0100

    dev

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 # backend "s3" {
 #   bucket               = "surepay-tfstate"
-#   workspace_key_prefix = "canary-eks-ghactions"
-#   key                  = "canary-eks-ghactions"
-#   region               = "us-east-1"
+#   workspace_key_prefix = "surepay-ecs"
+#   key                  = "surepay-ecs"
+#   region               = "eu-west-1"
 #   dynamodb_table       = "surepay-tfstate-lock"
 #   encrypt              = true
 # }
